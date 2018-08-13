f03b5402b009f64947f8e6202cc757bb48690dfc
MatthewWong <1298331581@qq.com>
2018-08-13 14:26:55 +0800

    .

--- application/index/controller/login.php
+++ application/index/controller/login.php
@@ -21,8 +21,7 @@ class login extends Controller
     {
         if (Request::instance()->isPost()) { //判断是否是是否为 POST 请求
             $data = input('post.');
-//            {admin_name: "admin", password: "123456", captcha: "m4r"}
-            $username = $data['admin_name'];
+            $username = $data['uname'];
             $password = md5($data['password']);
             $captcha = $data['captcha'];
             //调用验证器
@@ -33,44 +32,19 @@ class login extends Controller
                 return json($validate->getError());
             }
             //查询数据试库
-            $where['admin_name'] = $username;
+            $where['uname'] = $username;
             $where['status'] = 1;
             $userInfo = Db::name('admin')->where($where)->find();
             if ($userInfo && $userInfo['password'] === $password) {
                 //登录成功写入session
                 Session::set('admin_id', $userInfo['id']);
-                Session::set('admin_name', $userInfo['admin_name']);
+                Session::set('admin_name', $userInfo['uname']);
                 return json('jr');
             } else {
                 return json('用户名或密码错误!');
             }
         } else {
             return $this->fetch('login');
-        }
-    }
-
-    public function loginValidate()
-    {
-        // 验证规则
-        protected
-        $rule = [
-            'username|用户名' => 'require',
-            'password' => 'require',
-            'captcha' => 'require|checkCaptcha:null',
-        ];
-        protected
-        $message = [
-            'captcha.require' => '验证码错误!',  //验证码空的时候验证的它
-        ];
-        protected
-        function checkCaptcha($value)  //验证码输入的进验证的它
-        {
-            $captcha = new Captcha();
-            if ($captcha->check($value)) {
-                return true;
-            } else {
-                return '验证码错误!';
-            }
         }
     }
 }
